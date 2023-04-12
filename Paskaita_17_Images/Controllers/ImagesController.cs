3453cb3d6e4109c4601e1ff19fbf9e306ffc0746
DaumaG <dgarnys18@gmail.com>
2023-04-12 19:31:06 +0300

    initial

---
+++ Paskaita_17_Images/Controllers/ImagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paskaita_17_Images.DTO;
+using System.IO;
+
+namespace Paskaita_17_Images.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult UploadImage([FromForm] ImageUploadRequest imageUploadRequest)
+        {
+            using var memoryStream = new MemoryStream();
+            imageUploadRequest.Image.CopyTo(memoryStream);
+
+            byte[] imageBytes = memoryStream.ToArray();
+            return Ok();
+        }
+    }
+}
---
+++ Paskaita_17_Images/DTO/ImageUploadRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Paskaita_17_Images.Attributes;
+
+namespace Paskaita_17_Images.DTO
+{
+    public class ImageUploadRequest
+    {
+        [MaxFileSize(5 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".png", ".jpg", ".jpeg" })]
+        public IFormFile Image { get; set; }
+    }
+}
